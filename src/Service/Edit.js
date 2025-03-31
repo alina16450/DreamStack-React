ae12918e9f839e262bf6f81bf73c772205abccf2
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-03-31 18:59:27 +0300

    Update Edit.js

--- src/Service/Edit.js
+++ src/Service/Edit.js
@@ -4,9 +4,9 @@ import { useBucket } from '../Repository/BucketContext';
 import './Repo.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
+//contains all of the logic for editing/deleting an item.
 export default function Edit () {
-    
+    //call bucketRepo to use the function from Repository. useState hooks to handle the selected item, existing data, and the input labels.
     const bucketRepo = useBucket();
     const [items, setItems] = useState([]);
     const [selectedItem, setSelectedItem] = useState(null);
@@ -19,10 +19,12 @@ export default function Edit () {
     });
     const [error, setError] = useState();
 
+    //when bucketRepo is called (functions such as edit or delete), the table is refreshed. used useEffect for efficiency.
       useEffect(() => {
         setItems(bucketRepo.getItems());
       }, [bucketRepo]);
 
+    //when an item is selected, its data is automatically used to fill the input labels, for convenience and better user experience.
     const handleSelectItem = (item) => {
         setSelectedItem(item);
         setFormData({
@@ -34,10 +36,14 @@ export default function Edit () {
         });
     };
 
+    //logic for the submit edit process.
     const handleUpdate = () => {
         if (!selectedItem) return;
+
+        //again checking that only letters are inputted for all except description.
         const textOnlyPattern = /^[A-Za-z\s]+$/;
-    
+
+        //ensure no fields are left empty.
         if (!formData.name || !formData.country || !formData.city || !formData.category || !formData.description) {
         setError('All fields are required');
         return;
@@ -56,11 +62,7 @@ export default function Edit () {
         setError('City must contain only letters and spaces');
         return;
         }
-    
-        if (!textOnlyPattern.test(formData.description)) {
-        setError('Description must contain only letters and spaces');
-        return;
-        }
+        //calls on our repository function that edits an item's data with the values entered.
         bucketRepo.updateItem(
           selectedItem.id,
           formData.name,
@@ -69,11 +71,12 @@ export default function Edit () {
           formData.category,
           formData.description
         );
-        
+
         setItems([...bucketRepo.getItems()]);
         setError('');
       };
 
+      //handles logic for deleting item, by getting id of item on row selected and calling on function from repository.
       const handleDelete = (id, e) => {
         if (e) e.stopPropagation();
         bucketRepo.deleteItem(id);
@@ -117,6 +120,7 @@ export default function Edit () {
       <div className="edit-container">
         <h2 className="edit-title">Edit mode</h2>
         <h3 className="edit-subtitle">Click on the entry to modify:</h3>
+        //initialize the table.
           <div className="body">
         <table>
           <thead>
@@ -130,6 +134,7 @@ export default function Edit () {
             </tr>
           </thead>
           <tbody>
+              //use .map to add each item attribute to its respective collumn. Also implement logic to detect selected item by linking onClick with a hook.
             {items.map(item => (
                 <tr key={item.id} onClick={() => handleSelectItem(item)}
                 className={selectedItem?.id === item.id ? 'selected-row' : ''}>
@@ -146,6 +151,7 @@ export default function Edit () {
             ))}
           </tbody>
         </table>
+        //temporarily create input fields for each attribute, which only appears when an item is clicked. Will soon update to allow editing directly on the table.
         {selectedItem && (
       <div className="edit-form">
         <h3>Editing: {selectedItem.name}</h3>
