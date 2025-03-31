fbb8ccc42c20a5a8d16d831d8fc4bb035d1f75b3
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-03-31 15:15:12 +0300

    Update BucketContext.js

--- src/Repository/BucketContext.js
+++ src/Repository/BucketContext.js
@@ -1,8 +1,11 @@
 import React, { createContext, useContext } from 'react';
 import BucketItemsRepository from '../Repository/Repo';
 
+//using createContext in order to initialize one Repo for each of my 3 pages, to avoid having to pass any changes between them. These children get passed in App.js where the separate pages
+//are initialized.
 const BucketContext = createContext();
 
+//initialize the repository class, and pass it to children.
 export const BucketProvider = ({ children }) => {
   const repository = new BucketItemsRepository();
   
@@ -13,6 +16,7 @@ export const BucketProvider = ({ children }) => {
   );
 };
 
+//creates a custom hook for our action for easier calling.
 export const useBucket = () => {
   return useContext(BucketContext);
 };
