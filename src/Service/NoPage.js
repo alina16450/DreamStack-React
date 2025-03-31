49a467a53f8dcd33bfac59fa8111306d64bb742b
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-03-31 19:11:50 +0300

    Update NoPage.js

--- src/Service/NoPage.js
+++ src/Service/NoPage.js
@@ -1,6 +1,10 @@
 const NoPage = () => {
     //if any other variations of the url are attempted, other than the 3 pages created, a 404 page is given.
-    return <h1>404</h1>;
+    return (<div className = "noPage">
+    <h1 className = "no-page">404</h1>
+            <h2>Page not found</h2>
+            </div>
+    );
   };
   
   export default NoPage;
